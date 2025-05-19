f3ef024eebd170c7d9ff812eb055a6f9de562e1f
ludwig-sewall <113376600+ludwig-sewall@users.noreply.github.com>
2025-05-19 16:08:05 +0200

    Adding some comments

--- models/orders_silver.sql
+++ models/orders_silver.sql
@@ -1,5 +1,19 @@
-select 
-* 
-from {{ source('tpch', 'orders_bronze') }}
-    where O_ORDERKEY is not null
-    and O_ORDER_DATE >= date '1990-01-01'
+WITH orders_bronze AS (
+  SELECT
+    *
+  FROM {{ source('tpch', 'orders_bronze') }}
+), filter_1 AS (
+  /* Filter relevant dates */
+  SELECT
+    *
+  FROM orders_bronze
+  WHERE
+    NOT o_orderkey IS NULL AND o_order_date >= CAST('1990-01-01' AS DATE)
+), orders_silver AS (
+  SELECT
+    *
+  FROM filter_1
+)
+SELECT
+  *
+FROM orders_silver
